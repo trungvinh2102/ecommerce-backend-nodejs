bdfd732bd242359d945dc38a9f565b0be75a32ec
trungvinh02 <work.trungvinh@gmail.com>
2024-05-11 22:51:19 +0700

    Add Handle Success

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,12 +1,16 @@
 'use strict'
 
 const { AccessService } = require("../services/access.service");
+const { CREATED } = require('../core/success.response')
 
 class AccessController {
 
   signUp = async (req, res, next) => {
 
-    return res.status(201).json(await AccessService.signUp(req.body))
+    new CREATED({
+      message: "REGISTERED OK!",
+      metadata: await AccessService.signUp(req.body)
+    }).send(res)
   }
 }
 
---
+++ src/core/success.response.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const { StatusCodes, ReasonPhrases } = require('../utils/httpStatusCode')
+
+class SucessResponse {
+
+  constructor({ message, statusCode = StatusCodes.OK, reasonStatusCode = ReasonPhrases.OK, metadata = {} }) {
+    this.message = !message ? reasonStatusCode : message
+    this.statusCode = statusCode
+    this.metadata = metadata
+  }
+
+  send(req, headers = {}) {
+    return res.status(this.status).json(this)
+  }
+}
+
+class OK extends SucessResponse {
+
+  constructor({ message, metadata }) {
+    super({ message, metadata })
+  }
+}
+
+class CREATED extends SucessResponse {
+
+  constructor({ message, statusCode = StatusCodes.CREATED, reasonStatusCode = ReasonPhrases.CREATED, metadata }) {
+    super({ message, statusCode, reasonStatusCode, metadata })
+  }
+}
+
+module.exports = {
+  OK,
+  CREATED
+}
